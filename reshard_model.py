2ab42da84ea5ef6f164a561020f3b4250ddcdcaa
dmahan93 <44207705+dmahan93@users.noreply.github.com>
2023-03-03 17:39:31 -0600

    Update reshard_model.py to not OOM with 1 A100 80GB
    cpu, not cuda:0

--- reshard_model.py
+++ reshard_model.py
@@ -16,24 +16,28 @@ mappings = {
 }
 discard = "inner_attention"
 if __name__ == '__main__':
-    original_dir = "13B"
+    original_dir = "models/30B"
     model_shards = list()
     output_shards = dict()
-    for i in range(2):
-        model_shards.append(torch.load(f"{original_dir}/consolidated.{i:02}.pth"))
-    for key in list(model_shards[0].keys()):
-        if discard in key:
-            continue
-        for map in list(mappings.keys()):
-            if map in key:
-                if mappings[map] == -1:
-                    output_shards[key] = model_shards[0][key]
-                elif mappings[map] == 0:
-                    output_shards[key] = torch.cat(list([model_shards[i][key] for i in range(len(model_shards))]), dim=0)
-                elif mappings[map] == 1:
-                    output_shards[key] = torch.cat(list([model_shards[i][key] for i in range(len(model_shards))]), dim=1)
-        print(key, model_shards[0][key].shape)
+    for i in range(4):
+        model_shards.append(torch.load(f"{original_dir}/consolidated.{i:02}.pth", "cpu"))
+    with torch.no_grad():
+        for key in list(model_shards[0].keys()):
+            if discard in key:
+                continue
+            for map in list(mappings.keys()):
+                if map in key:
+                    if mappings[map] == -1:
+                        output_shards[key] = model_shards[0][key]
+                    elif mappings[map] == 0:
+                        output_shards[key] = torch.cat(list([model_shards[i][key] for i in range(len(model_shards))]), dim=0)
+                    elif mappings[map] == 1:
+                        output_shards[key] = torch.cat(list([model_shards[i][key] for i in range(len(model_shards))]), dim=1)
+            print(key, model_shards[0][key].shape)
+            for i in range(len(model_shards)):
+                del model_shards[i][key]
+
     print("-------------------")
     for key in list(output_shards.keys()):
-        print(key, model_shards[0][key].shape)
+        print(key, output_shards[key].shape)
     torch.save(output_shards, f"{original_dir}/single.pth")
